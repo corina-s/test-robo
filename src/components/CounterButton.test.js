7b6501f76a95ebf29b119e473c6f0ebb0b797492
corina <corina.schambacher@gmail.com>
2021-04-16 08:29:00 -0700

    counterButton tests

---
+++ src/components/CounterButton.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import CounterButton from './CounterButton';
+
+it('expect to render CounterButton component', ()=>{
+  const mockColor = 'red'
+  expect(shallow(<CounterButton color={mockColor}/>)).toMatchSnapshot();
+});
+it('correctly increments the counter', ()=>{
+  const mockColor = 'red';
+  const wrapper = shallow(<CounterButton color={mockColor}/>);
+  wrapper.find('[id="counter"]')
+});
+
