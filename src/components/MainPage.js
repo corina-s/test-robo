8e75b728393530130191c701e3cf47572b9a1f39
corina <corina.schambacher@gmail.com>
2021-04-28 17:45:06 +0200

    filterrobots test

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -13,12 +13,14 @@ class MainPage extends Component {
   componentDidMount() {
     this.props.onRequestRobots();
   }
-
-  render() {
-    const { robots, searchField, onSearchChange, isPending } = this.props;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  filterRobots = ()=> {
+    return this.props.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.props.searchField.toLowerCase());
     })
+  }
+  render() {
+    const { robots, onSearchChange, isPending } = this.props;
+    
     return (
       <div className='tc'>
         <Header/>
@@ -26,7 +28,7 @@ class MainPage extends Component {
         <Scroll>
           { isPending ? <h1>Loading</h1> :
             <ErrorBoundry>
-              <CardList robots={filteredRobots} />
+              <CardList robots={this.filterRobots()} />
             </ErrorBoundry>
           }
         </Scroll>
--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -11,9 +11,28 @@ beforeEach(()=>{
     searchField: '',
     isPending: false
   }
-  wrapper = shallow(<MainPage { ...mockProps}/>)
+  let wrapper = shallow(<MainPage { ...mockProps}/>)
 })
 it('renders MainPage without crashing', () => {
   
   expect(wrapper).toMatchSnapshot()
+})
+it('filters robots correctly', () => {
+  const mockProps2 = {
+    onRequestRobots: jest.fn(),
+    robots: [{
+      id: 3,
+      name: 'John',
+      email: 'john@gmail.com'
+    }],
+    searchField: 'john',
+    isPending: false
+  }
+  let wrapper2 = shallow(<MainPage {...mockProps2}/>)
+  // expect(wrapper2.instance().filterRobots()).toEqual([])
+  expect(wrapper2.instance().filterRobots()).toEqual([{
+    id: 3,
+    name: 'John',
+    email: 'john@gmail.com'
+  }])
 })
