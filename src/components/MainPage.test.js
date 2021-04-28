c2b6a5c0aae1b8d0006ee8c045619e7e36725196
corina <corina.schambacher@gmail.com>
2021-04-28 17:26:42 +0200

    mainPage test

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -5,12 +5,15 @@ import MainPage from './MainPage';
 let wrapper;
 
 beforeEach(()=>{
-  const mockProps = {}
-})
-it('expect to render MainPage Component', () => {
-  const mockStore = {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
     robots: [],
-    searchField: ''
+    searchField: '',
+    isPending: false
   }
-  expect(shallow(<MainPage store={mockStore}/>)).toMatchSnapshot()
+  wrapper = shallow(<MainPage { ...mockProps}/>)
+})
+it('renders MainPage without crashing', () => {
+  
+  expect(wrapper).toMatchSnapshot()
 })
